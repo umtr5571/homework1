52b305ea6abed2637cc348f66badf14d73abf335
umtr <akiakiyoshi55@gmail.com>
2018-11-19 13:20:34 +0900

    test plus fxc

--- main.cpp
+++ main.cpp
@@ -3,4 +3,18 @@
 
 using namespace std;
 
-int main
+int main(){
+    shisoku nyanya;
+    int a,b;
+
+    cout << "整数を入力してください \n整数a:" << endl;
+    cin >> a;
+    cout << "整数b:" << endl;
+    cin >> b;
+
+    cout << "a+b=" << nyanya.plus << endl;
+
+
+
+
+}
